1a508c17cb7bdfa3a6caedb2ac5d80a88970a45e
Bhagat Nath <bhagat.s.nath@gmail.com>
2026-02-13 21:37:29 -0500

    Practice 4 Update

--- Practice4.java
+++ Practice4.java
@@ -5,57 +5,38 @@
  */
 import java.util.*;
 public class Practice4 {
-    ArrayList<Integer> indexPlacement = new ArrayList<>();
-    ArrayList<Integer> maxConsec = new ArrayList<>();
-    static ArrayList<Character> charArray = new ArrayList<>();
-    int numConsec = 0;
-    boolean extraDayAvailable = true;
     public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        Practice4 p = new Practice4();
-        int n = sc.nextInt();
-        for (int i = 1; i<=n;i++){
-            char weather = sc.next().charAt(0);
-            charArray.add(weather);
-        }
-        p.sortingLogic(charArray);
-        System.out.println(p.outputLogic());
-    }
-    public void sortingLogic(ArrayList<Character> c){
-        for (int i =0; i<c.size();i++){
-            if (c.get(i)=='S'){
-                indexPlacement.add(i);
+        String testString = "PSPSSPPS";
+        //String testString = "SPPSS";
+
+        int left = 0;
+        int right = 0;
+        int pCount = 0;
+        int maxLength = 0;
+
+        int maxSubStringStart = 0;
+        int maxSubStringEnd = 0;
+
+        for(right = 0; right < testString.length(); right++) {
+            if(testString.charAt(right) == 'P') {
+                pCount++;
             }
-        }
-    }
-    public int outputLogic(){
-        int i =0;
-        while (i<indexPlacement.size()){
-            int a = indexPlacement.get(i);
-            numConsec++;
-            try{
-                if (!(indexPlacement.get(i+1)==a+1)){
-                    if (indexPlacement.get(i+1)==a+2&&extraDayAvailable){
-                        numConsec++;
-                        extraDayAvailable = false;
-                    }
-                    else{
-                        maxConsec.add(numConsec);
-                        numConsec = 0;
-                    }
+            while(pCount > 1) {
+                if(testString.charAt(left) == 'P') {
+                    pCount--;
                 }
+                left++;
             }
-            catch (Exception e){
-                
+
+            if(maxLength < (right - left + 1)) {
+                maxLength = right - left + 1;
+                maxSubStringStart = left;
+                maxSubStringEnd = right;
+
             }
-            i++;
+            //maxLength = Math.max(maxLength, right - left + 1);
         }
-        int max = maxConsec.get(0); 
-        for (int a =0; i<maxConsec.size();a++){
-            if (max<maxConsec.get(a)){
-                max = maxConsec.get(a);
-            }
-        }
-        return max;
+        System.out.println(maxLength);
+
     }
 }
