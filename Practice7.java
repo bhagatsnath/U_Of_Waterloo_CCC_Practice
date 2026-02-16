6a1aa7e788b4d80e77844c69b73092517138b028
Bhagat Nath <bhagat.s.nath@gmail.com>
2026-02-15 19:23:01 -0500

    Updated Logic for Question 3 2024

--- Practice7.java
+++ Practice7.java
@@ -1,43 +1,27 @@
 /**
  * Author: Bhagat Sabari Nath
  * Date: Feb 15, 2026
- * Problem 3
+ * Question 3 2024
  */
 import java.util.*;
 public class Practice7 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        ArrayList <Integer> scores = new ArrayList<>();
+        HashMap<Integer,Integer> scores = new HashMap<>();
         int n = sc.nextInt();
         for (int i =0; i<n;i++){
-            scores.add(sc.nextInt());
-        }
-        int max = findMax(scores);
-        scores.removeAll(Arrays.asList(max));
-        max = findMax(scores);
-        // List<Integer> myList = new ArrayList();
-        // myList.add(max);
-        scores.removeAll(Arrays.asList(max));
-        max = findMax(scores);
-        int countOfBronze = 0;
-        int b = 0;
-        while (b<scores.size()){
-            if (scores.get(b)==max){
-                countOfBronze++;
+            Set<Integer> keySet= scores.keySet();
+            int scoreInput = sc.nextInt();
+            if (keySet.contains(scoreInput)){
+                int numOfPeople = scores.get(scoreInput);
+                scores.put(scoreInput,numOfPeople+1);
             }
-            b++;
-        }
-        System.out.println(max+" "+countOfBronze);
-    }
-    public static int findMax(ArrayList<Integer> scores){
-        int a = 0;
-        int max = scores.get(0);
-        while(a<scores.size()){
-            if (scores.get(a)>max){
-                max = scores.get(a);
+            else{
+                scores.put(scoreInput,1);
             }
-            a++;
         }
-        return max;
+        List<Integer> keyList = new ArrayList<>(scores.keySet());
+        Collections.sort(keyList, Collections.reverseOrder());
+        System.out.println(keyList.get(2)+" "+scores.get(keyList.get(2)));
     }
 }
